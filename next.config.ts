15cd84b2181398c6ccb031b51fe871cebd218ad3
Jinoko01 <dswvgw6412@naver.com>
2026-05-26 15:35:23 +0900

    to reproduce a catch bug

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [
+      'remark-plugin-does-not-exist', // ← 없는 플러그인 경로
+    ],
+  },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
